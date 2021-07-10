941279d60cce99ba76e02bb45aabbf7103d662d7
Paahn <paahndev@gmail.com>
2021-07-09 22:19:02 -0700

    alias assigned to element, passed into Input() and used for the binding

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() public element: { type: string, name: string, content: string };
+  @Input('servElement') public element: { type: string, name: string, content: string };
 
   constructor() { }
 
